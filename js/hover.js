9aad85cb979008862efa63e5d501fa602bbc9f84
Kevin Tan <kevin.tan@yale.edu>
2014-11-18 18:31:48 -0500

    finally fixed centering for categories page with display table

--- js/hover.js
+++ js/hover.js
@@ -1,29 +1,61 @@
-$(window).load(function(){
-// hover show text
+// $(window).load(function(){
+
+//   // use if you want the reverse (hidden until mouseover)
+//   // $(document).on('mouseover',function(e) {
+//   //   var id_pic = $(e.target).attr('id');
+//   //   var class_pic  = $(e.target).attr('class');
+//   //   if (class_pic=="category"){
+//   //     if (id_pic=="iceland-pic"){
+//   //       $("#iceland-text").show();
+//   //       $("#israel-text").hide();
+//   //       $("#art-text").hide();
+//   //       $("#south-text").hide();
+//   //     }
+//   //     if (id_pic=="israel-pic"){
+//   //       $("#israel-text").show();
+//   //       $("#iceland-text").hide();
+//   //       $("#art-text").hide();
+//   //       $("#south-text").hide();
+//   //   }
+//   //     if (id_pic=="art-pic"){
+//   //       $("#art-text").show();
+//   //       $("#iceland-text").hide();
+//   //       $("#israel-text").hide();
+//   //       $("#south-text").hide();
+//   //     }
+//   //     if (id_pic=="south-pic"){
+//   //       $("#south-text").show();
+//   //       $("#iceland-text").hide();
+//   //       $("#israel-text").hide();
+//   //       $("#art-text").hide();
+//   //     }
+//   //   }
+//   //   else{
+//   //     $("#iceland-text").hide();
+//   //     $("#israel-text").hide();
+//   //     $("#art-text").hide();
+//   //     $("#south-text").hide();
+//   //   }
+//   // });
 
 
-	//size text 
-	height = $(".category").height()/2.8;
-	height_painting = $(".category").height()/6;
-	width = $(".category").width();
+//   height = $(".category").height()/2.8;
+//   width = $(".category").width();
 
-	$(".work-text").css('margin-top', height+'px')
-	$("#painting-text").css('margin-top', height_painting+'px')
-	$(".work-text").css('width', width+'px')
+//   $(".work-text").css('margin-top', height+'px')
+//   $(".work-text").css('width', width+'px')
 
-	// size text on resize
-	$(window).resize(function(){
-	  height = $(".category").height()/2.8;
-	$(".work-text").css('margin-top', height+'px')
-	});
-	$(window).resize(function(){
-	  height_painting = $(".category").height()/6;
-	  $("#painting-text").css('margin-top', height_painting+'px')
-	});
-	//size width text-align:center works!
-	$(window).resize(function(){
-	  width = $(".category").width();
-	  $(".work-text").css('width', width+'px')
-	});
+//   // size text 
+//   $(window).resize(function(){
+//     height = $(".category").height()/2.8;
+//   $(".work-text").css('margin-top', height+'px')
+//   });
+//   //size width text-align:center works!
 
-});
+
+//   $(window).resize(function(){
+//     width = $(".category").width();
+//     $(".work-text").css('width', width+'px')
+//   });
+
+// });
